1d5038a66b9e18188275a0a03843226c7c2b9ca1
ocelot2123 <edwardfu850808@gmail.com>
2023-05-01 19:10:01 +0200

    init project and add discord slash command listener

---
+++ apps/discord/src/app.ts
@@ -0,0 +1,58 @@
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+import path from "path";
+import fs from "fs";
+
+declare module "discord.js" {
+  interface Client {
+    commands: Collection<string, any>;
+  }
+}
+
+dotenv.config();
+
+export const token = process.env.DISCORD_TOKEN;
+export const clientId = "1102628311378710628";
+
+console.log("Bot is starting...");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".ts"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".ts"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
+client.login(token);
